9e8b7a9539bb6ed3cdaa1fbf0163c80ec21a55b1
sqoobe <sqoobe96@gmail.com>
2025-04-24 13:26:05 +0200

    sync function to get dog img

--- Async JavaScript & API/index.js
+++ Async JavaScript & API/index.js
@@ -1,4 +1,18 @@
-fetch("https://apis.scrimba.com/dog.ceo/api/breeds/image/random")
+async function getDogImage() {
+  const response = await fetch(
+    "https://apis.scrimba.com/dog.ceo/api/breeds/image/random"
+  );
+  const data = await response.json();
+  const imageElement = document.createElement("img");
+  imageElement.src = data.message;
+  imageElement.alt = "random dog picture";
+  document.getElementById("img-container").appendChild(imageElement);
+  console.log(data);
+}
+
+getDogImage();
+
+/* fetch("https://apis.scrimba.com/dog.ceo/api/breeds/image/random")
   .then((response) => response.json())
   .then((data) => {
     const imageElement = document.createElement("img");
@@ -6,3 +20,4 @@ fetch("https://apis.scrimba.com/dog.ceo/api/breeds/image/random")
     imageElement.alt = "random dog picture";
     document.getElementById("img-container").appendChild(imageElement);
   });
+*/
