c255cd5b2bcac855afe77cc9b99407a9364e549b
sqoobe <sqoobe96@gmail.com>
2025-04-24 14:46:16 +0200

    promise challenge

--- Async JavaScript & API/index.js
+++ Async JavaScript & API/index.js
@@ -21,3 +21,20 @@ getDogImage();
     document.getElementById("img-container").appendChild(imageElement);
   });
 */
+const promise = new Promise((resolve, reject) => {
+  const success = Math.random() > 0.5;
+  if (success) {
+    resolve("Operation successful");
+  } else {
+    reject("Operation failed");
+  }
+});
+
+// promise.then(response => console.log(response))
+
+try {
+  const response = await promise;
+  console.log(response);
+} catch (err) {
+  console.log(err);
+}
