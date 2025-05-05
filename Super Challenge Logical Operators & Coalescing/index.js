03ea41b8f02a75c3005f4c53d7cdff50c1332bd6
sqoobe <sqoobe96@gmail.com>
2025-05-05 14:11:20 +0200

    Super Challenge Library Software done

--- Super Challenge Logical Operators & Coalescing/index.js
+++ Super Challenge Logical Operators & Coalescing/index.js
@@ -17,29 +17,35 @@ const collection = [];
 
 function addBookToCollection(title, author, yearPublished, libraryData) {
   // Explicitly check for title
-  if (title === undefined || title === null || title === "") {
-    title = "Unknown Title";
-  }
+  //   if (title === undefined || title === null || title === "") {
+  //     title = "Unknown Title";
+  //   }
+  title = title || "Unknown Title";
 
   // Explicitly check for author
-  if (author === undefined || author === null || author === "") {
-    author = "Unknown Author";
-  }
+  //   if (author === undefined || author === null || author === "") {
+  //     author = "Unknown Author";
+  //   }
+  author = author || "Unknown Author";
 
   // Explicitly check if yearPublished is undefined or null
-  if (yearPublished === undefined || yearPublished === null) {
-    yearPublished = "Not Specified";
-  }
+  //   if (yearPublished === undefined || yearPublished === null) {
+  //     yearPublished = "Not Specified";
+  //   }
+  yearPublished = yearPublished ?? "Not Specified";
 
   // Explicitly check for availability at main library
-  let availability = "Not Available"; // Default value
-  if (libraryData) {
-    if (libraryData.locations) {
-      if (libraryData.locations.mainLibrary) {
-        availability = "Available";
-      }
-    }
-  }
+  //   let availability = "Not Available"; // Default value
+  //   if (libraryData) {
+  //     if (libraryData.locations) {
+  //       if (libraryData.locations.mainLibrary) {
+  //         availability = "Available";
+  //       }
+  //     }
+  //   }
+
+  const availability =
+    (libraryData?.locations?.mainLibrary && "Available") || "Not Available";
 
   // Push the book object to 'collection'
   collection.push({
